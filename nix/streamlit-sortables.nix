31b5728cffa0b7994f110ea3f458f17d187bf323
Carman Fu <huivioemp1bs@posteo.com>
2025-11-14 14:48:30 +0800

    chore(streamlit-sortables): drop propagatedBuildInputs

--- nix/streamlit-sortables.nix
+++ nix/streamlit-sortables.nix
@@ -1,6 +1,5 @@
 {
   buildPythonPackage,
-  streamlit,
   fetchurl,
 }:
 buildPythonPackage {
@@ -11,5 +10,4 @@ buildPythonPackage {
     url = "https://files.pythonhosted.org/packages/3d/b4/edbef9fa8e49f5df2745a178c44b23553d8472c22fb23f44f6949d544410/streamlit_sortables-0.3.1-py3-none-any.whl";
     hash = "sha256-GEey+iBBditkQuISJL2MAxbFQS5Os6OPp0jPfO2DTUk=";
   };
-  propagatedBuildInputs = [ streamlit ];
 }
